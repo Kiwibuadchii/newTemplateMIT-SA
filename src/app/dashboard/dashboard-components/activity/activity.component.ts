701e997aa7106a6b96cddf05acc73f9696d92549
Sarayut <sarayutj.tong@gmail.com>
2024-09-18 20:31:42 +0700

    bg

--- src/app/dashboard/dashboard-components/activity/activity.component.ts
+++ src/app/dashboard/dashboard-components/activity/activity.component.ts
@@ -9,6 +9,7 @@ interface activity {
 @Component({
   selector: 'app-activity',
   templateUrl: './activity.component.html',
+  styleUrls: ['./activity.component.scss']
 })
 export class ActivityComponent implements OnInit {
 
